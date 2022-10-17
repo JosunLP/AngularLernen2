6724a20a6ee98d70c5f5d24bc803324d5bd834f4
Jonas Pfalzgraf <jp@jamoin.de>
2022-10-17 11:14:03 +0200

    Modul 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { WelcomeComponent } from './home/welcome.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -7,10 +8,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/pipes/convertToSpaces.pipe';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
+    { path: 'products', component: ProductListComponent },
+    { path: 'products/:id', component: ProductDetailComponent },
+    { path: 'welcome', component: WelcomeComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent },
+  ])],
   providers: [],
   bootstrap: [AppComponent],
 })
