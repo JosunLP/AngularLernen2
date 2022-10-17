12391ed9e1ceaaf0c971d9957f149d80aea7f883
Jonas Pfalzgraf <jp@jamoin.de>
2022-10-17 10:36:43 +0200

    Modul 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { StarComponent } from './shared/star/star.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -9,7 +10,7 @@ import { ConvertToSpacesPipe } from './shared/pipes/convertToSpaces.pipe';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,27 +1,26 @@
 import { Product } from './../shared/types/product.type';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProductService } from '../shared/services/product-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.sass'],
 })
-export class ProductListComponent implements OnInit {
-
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Product List';
   private _listFilter: string = '';
-  filteredProducts: Product[];
-  products: Product[];
+  filteredProducts: Product[] = [];
+  products: Product[] = [];
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
   ratingClicked: string = '';
+  private errorMessage: string = '';
+  sub!: Subscription;
 
-  constructor(private productService: ProductService) {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
-  }
+  constructor(private productService: ProductService) {}
 
   get listFilter(): string {
     return this._listFilter;
@@ -38,7 +37,19 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('In OnInit');
+    this.sub = this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: (err) => (this.errorMessage = err),
+      complete: () => console.log('Completed'),
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+    console.log('Destroying');
   }
 
   private performFilter(filterBy: string): Product[] {
